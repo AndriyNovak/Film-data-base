02c667cf8586aba734c4300467a1d9306588c215
Andriy <cnayper13@gmail.com>
2019-07-11 14:56:37 +0300

    next2 commit

--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html"
     })
-  ]
+  ],
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  }
 };
